25909cdbfb19d127867b2773337d1d7d146193ed
mohammed-alwasy <445052803@std.psau.edu.sa>
2026-04-29 19:49:09 +0300

    completed task 1

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock; // task 1
 
 // ANSI Color Codes for enhanced terminal output
 class Colors {
@@ -37,35 +38,49 @@ class SharedResources {
     public static List<String> executionLog = new ArrayList<>();  // Shared list - NEEDS PROTECTION!
     
     // TODO #1: Add a ReentrantLock(s) here to protect critical sections
-    // Example: public static final ReentrantLock lock = new ReentrantLock();
+    public static final ReentrantLock lock = new ReentrantLock();
     
     // TODO #2: Add a Semaphore to limit concurrent process execution
     // Example: public static final Semaphore cpuSemaphore = new Semaphore(1);
     
     // Method to increment context switch counter
     public static void incrementContextSwitch() {
-        // TODO: Protect this critical section with a lock
-        // RACE CONDITION: Multiple threads might read and write simultaneously!
-        contextSwitchCount++;
+        lock.lock();    // task 1
+        try {
+            contextSwitchCount++;
+        } finally {      
+            lock.unlock();  // task 1
+        }
     }
     
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
-        // TODO: Protect this critical section with a lock
-        completedProcessCount++;
+       lock.lock();    // task 1
+        try {
+            completedProcessCount++;
+        } finally {
+            lock.unlock(); // task 1
+        }
     }
     
     // Method to add waiting time
     public static void addWaitingTime(long time) {
-        // TODO: Protect this critical section with a lock
-        totalWaitingTime += time;
+        lock.lock(); // task 1
+        try {
+            totalWaitingTime += time;
+        } finally {
+            lock.unlock(); // task 1
+        }
     }
     
     // Method to log execution
     public static void logExecution(String message) {
-        // TODO: Protect this critical section with a lock
-        // RACE CONDITION: ArrayList is not thread-safe!
-        executionLog.add(message);
+        lock.lock(); // task 1
+        try {
+            executionLog.add(message);
+        } finally {
+            lock.unlock(); // task 1
+        }
     }
 }
 
